14578f1e4a50594bb925eea0ac1f42424ec81c26
Vlad Palos <vladpalos@vladpalos-ThinkPad-T510.(none)>
2013-09-28 10:39:11 +0300

    Created fresh rails app.

---
+++ lifegoal/config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Lifegoal::Application.config.secret_key_base = 'e81dd3a3864a35da8189a04e9682ef0fdd1b7ad84f716610fcfe0d1e21a81488e2014814d49dbe59a2a2ca766ff9638e5c0e48c1b8736326c1d9a8fc5a196fe1'
